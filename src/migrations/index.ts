213f70bce983ad85f11d55e091b863210a1ed5da
tansamuel <tan@atech.software>
2026-05-05 21:29:12 +0700

    db

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260505_130756 from './20260505_130756';
-
-export const migrations = [
-  {
-    up: migration_20260505_130756.up,
-    down: migration_20260505_130756.down,
-    name: '20260505_130756'
-  },
-];
